4bed34a4cdfe4171206b4e41e37b7a4b87b27cd7
ArconRM <85395885+ArconRM@users.noreply.github.com>
2025-10-04 15:00:25 +0400

    fix: fixed images processing logic

--- models-inference/inference.py
+++ models-inference/inference.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel, Field
 from ultralytics import YOLO
 import os, cv2, glob
 import torch
+from pathlib import Path
 
 torch.set_num_threads(2)
 torch.set_num_interop_threads(2)
@@ -34,7 +35,8 @@ async def crop_to_license_plates(req: CropToLicensePlatesRequest):
     crop_index = 0
     overall_index = 0
     for fp in frame_paths:
-        print("Processing frame #", overall_index)
+        time = Path(fp).stem.split("_")[1]
+        print("Processing frame #", overall_index, time)
         img = cv2.imread(fp)
         if img is None:
             continue
@@ -44,11 +46,11 @@ async def crop_to_license_plates(req: CropToLicensePlatesRequest):
             for box in r.boxes:
                 x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
                 crop = img[y1:y2, x1:x2].copy()
-                outp = os.path.join(result_data_dir, f"crop_{crop_index:06d}.png")
+                outp = os.path.join(result_data_dir, f"crop_{time}.png")
                 cv2.imwrite(outp, crop)
                 crop_index += 1
 
         overall_index += 1
 
-    print("Detection+crop finished. Crops saved to", result_data_dir)
+    print("Detection+crop finished.", crop_index, "Crops saved to", result_data_dir)
     return {"status": "ok", "crops_saved": crop_index}
